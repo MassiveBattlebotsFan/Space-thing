38cf472bad78c09d0e93bc124c940cf63e63fd1d
MassiveBattlebotsFan <teamlightning2018@outlook.com>
2022-05-09 15:31:58 -0500

    Changed way button animation moves things

--- Scripts/Button.gd
+++ Scripts/Button.gd
@@ -6,6 +6,6 @@ func clicked():
 	toggle *= -1
 	print(toggle)
 	if toggle > 0:
-		$MeshInstance/Button_anim.play("Button_anim")
+		$CollisionShape/Button_anim.play("Button_anim")
 	else:
-		$MeshInstance/Button_anim.play_backwards("Button_anim")
+		$CollisionShape/Button_anim.play_backwards("Button_anim")
